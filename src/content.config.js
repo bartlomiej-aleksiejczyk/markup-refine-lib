17d20e2c2f7b08ae685ff11fa4b4497ef3bea656
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-04-01 21:18:28 +0200

    refactor: fix store for astro components

--- src/content.config.js
+++ src/content.config.js
@@ -4,11 +4,11 @@ import { glob } from "astro/loaders";
 const rawComponents = defineCollection({
   loader: () => {
     const data = Object.keys(
-      import.meta.glob("./pages/rawComponents/*.html", { eager: true })
+      import.meta.glob("./pages/rawComponents/*.astro", { eager: true })
     );
     return data.map((path) => ({
       id: path.split("/").pop(),
-      url: path.split("/").slice(2).join("/"),
+      url: path.split("/").slice(2).join("/").split(".")[0],
     }));
   },
 });
