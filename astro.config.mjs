0f756d9a5d4f7467d84ae956e7f6ed647684a942
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-04-03 22:58:52 +0200

    docs: fix astro docs generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   outDir: "./docs",
+  site: "https://bartlomiej-aleksiejczyk.github.io",
+  base: "markup-refine-lib",
 });
