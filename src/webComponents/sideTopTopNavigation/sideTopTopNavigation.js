e7932f0b5077d927026f89feee33efe566f3e913
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-04-13 19:04:50 +0200

    style: fix responsive container behavior ands webcomponent compatibility

--- src/webComponents/sideTopTopNavigation/sideTopTopNavigation.js
+++ src/webComponents/sideTopTopNavigation/sideTopTopNavigation.js
@@ -189,7 +189,7 @@ class SideTopTopNavigation extends HTMLElement {
           }
         }
 
-        @media (max-width: 768px) {
+        @media (max-width: 992px) {
           .primary-sidebar{
             transition: transform 0.3s ease-in-out;
             height: 100vh;
