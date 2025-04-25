19db2ecbb41e792301d7f59e9bc08822932db7ff
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-04-25 23:29:36 +0200

    style: add side bar way to arrange main navigation

--- src/attributeComponents/markup-refine-lib-attribute-components.ts
+++ src/attributeComponents/markup-refine-lib-attribute-components.ts
@@ -4,6 +4,8 @@ import "./sideTopTopNavigation/sideTopTopNavigation.css";
 import { initCopyableSnippet } from "./copyableSnippet/copyableSnippet";
 import "./copyableSnippet/copyableSnippet.css";
 
+import "./clickableItemList/clickableItemList.css"
+
 document.addEventListener("DOMContentLoaded", () => {
   initSideTopTopNavigation();
   initCopyableSnippet();
