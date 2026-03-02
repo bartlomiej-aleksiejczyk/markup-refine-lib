a8a3c5fe665cd805d90f1ad76557b5b137e5fb14
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2026-03-02 23:12:40 +0100

    ops: fix docs building script

--- src/build/buildDocs.js
+++ src/build/buildDocs.js
@@ -1,3 +1,12 @@
+import fs from "fs";
+import path from "path";
 import { createSearchIndex } from "./createSearchIndex.js";
 
+const nojekyllPath = path.join(process.cwd(), "docs", ".nojekyll");
+try {
+  fs.writeFileSync(nojekyllPath, "");
+  console.log(".nojekyll file created in docs/");
+} catch (err) {
+  console.error("Failed to write .nojekyll file:", err);
+}
 await createSearchIndex();
